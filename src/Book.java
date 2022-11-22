b05fc0c8b13c64451755e0eb247b01137ca36afe
tatiana_alekseeva <tatianaalekseeva1313@gmail.com>
2022-11-22 19:40:28 +0300

    ДЗ

--- src/Book.java
+++ src/Book.java
@@ -1,3 +1,22 @@
 public class Book {
-
+    private String name;
+    private int yearPublication;
+    Author author;
+    public Book(String name, int yearPublication, Author author) {
+        this.name = name;
+        this.yearPublication = yearPublication;
+        this.author = author;
+    }
+    public String getName() {
+        return this.name;
+    }
+    public int getYearPublication() {
+        return this.yearPublication;
+    }
+    public void setYearPublication(int yearPublication) {
+        this.yearPublication = yearPublication;
+    }
+    public Author getAuthor() {
+        return author;
+    }
 }
